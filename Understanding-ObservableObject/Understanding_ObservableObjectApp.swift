d0d07f0a62b910c0cf92c998faee048f3de3bf9a
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2023-04-21 08:56:21 -0500

    Initial Commit

---
+++ Understanding-ObservableObject/Understanding_ObservableObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Understanding_ObservableObjectApp.swift
+//  Understanding-ObservableObject
+//
+//  Created by Oscar David Myerston Vega on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Understanding_ObservableObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
